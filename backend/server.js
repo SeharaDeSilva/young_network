4679866c894ff0b14f8f1b021389bad2263dfd7a
IT21159008 <99635852+IT21159008@users.noreply.github.com>
2024-03-19 21:16:16 +0530

    comit2

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,35 @@
-//import modules
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const morgan = require('morgan');
 const cors = require('cors');
-require("dotenv").config();
-
-//app
+const cookieParser = require('cookie-parser');
+ 
 const app = express();
-
-//db
-
-
-//middleware
-app.use(morgan("dev"));
-app.use(cors({origin:true, credentials:true}));
-
-//routes
-
-//port
-const port = process.env.PORT || 8080;
-
-//listener
-const server = app.listen(port, ()=>console.log('Server is running on port ${port}'));
-
+ 
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+ 
+const port = process.env.PORT || 5000;
+const URL = process.env.MONGO_URI;
+ 
+mongoose.connect(URL, {
+   // useCreateIndex: true,
+   // useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+ 
+mongoose.connection.on('connected', () => {
+    console.log('MongoDB connected successfully');
+});
+ 
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to MongoDB:', err);
+});
+ 
+app.get('/', (req, res) => {
+    res.status(500).send("hello world");
+});
+ 
+app.listen(port, () => console.log(`app is running on ${port}`));
