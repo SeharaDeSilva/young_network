da0abff47fe47858de30c15aa912d3f76fa6786b
Balasooriya R.C <99980850+it21348648@users.noreply.github.com>
2024-03-20 22:18:28 +0530

    start frontend

---
+++ frontend/src/scenes/ravindu/add_service.js
@@ -0,0 +1,19 @@
+import './add_service.css'
+//import { useState, useEffect } from 'react'
+import axios from 'axios'
+//import serviceIMG from './Images/serviceIMG1.png';
+
+
+
+export default function Add_service() {
+    return (
+       /* <div>
+            <img src={serviceIMG} alt="cover" />
+
+        </div>*/
+
+        <div className="background-square-container">
+
+        </div>
+    )
+}
