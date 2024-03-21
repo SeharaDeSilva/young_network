cb53b09d52f4b023849c20688e7ff46ebcdf8e35
Sehara De Silva <99351913+SeharaDeSilva@users.noreply.github.com>
2024-03-21 10:16:27 +0530

    commit changes

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -3,7 +3,7 @@ const auth = require('../middlewares/auth');
 const userController = require('../controllers/userController')
 
 //router.get('/search',auth, userController.searchUser)
-router.get('/user/:id',auth, userController.getUser);
+router.get('/user/id',auth, userController.getUser);
 router.patch('/user',auth,userController.updateUser);
 
 module.exports = router;
